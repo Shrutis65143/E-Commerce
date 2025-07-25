a903c059cd4449e5b29e5a586a37f530a0eac36b
Shruti Singh <168437828+Shrutis65143@users.noreply.github.com>
2025-07-25 21:06:43 +0530

    Update BestSeller.jsx

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -21,7 +21,7 @@ const BestSeller = () => {
         <div className='text-center text-3xl py-8'>
             <Title text1={'BEST'} text2={'SELLERS'}/>
             <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
-            Our best sellers feature top-rated, customer-favorite products known for quality, value, and style. These must-have items are flying off the shelves—shop now before they’re gone!
+            Our best sellers feature top-rated, customer-favorite products known for quality, value, and style. These must-have items are flying off the shelves—shop now before they’re gone!!
 
             </p>
         </div>
