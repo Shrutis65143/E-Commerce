68bc8a203bbf4ce1acf6f536dd3fc9f736bb2bd6
Shruti Singh <168437828+Shrutis65143@users.noreply.github.com>
2025-07-25 21:21:23 +0530

    Update NewsletterBox.jsx

--- frontend/src/components/NewsletterBox.jsx
+++ frontend/src/components/NewsletterBox.jsx
@@ -12,7 +12,7 @@ const NewsletterBox = () => {
     <div className='text-center'>
       <p className='text-2xl font-medium text-gray-800'>Subscribe now & get 20% off</p>
       <p className='text-gray-400 mt-3'>
-      Subscribe now to receive exclusive offers, early access to new collections
+      Subscribe now to receive exclusive offers, early access to new collections.
       </p>
       <form  onSubmit={onSubmitHandler} className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3'>
         <input className='w-full sm:flex-1 outline-none' type="email"  placeholder='Enter your email' required/>
