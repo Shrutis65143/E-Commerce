dd518f7a9da7d2f7fad58aedcc7f53684f20c36d
Shruti Singh <168437828+Shrutis65143@users.noreply.github.com>
2025-07-25 20:45:56 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -36,7 +36,7 @@ const Footer = () => {
 
       <div>
         <hr />
-        <p className='py-5 text-sm text-center'>Copyright 2025@ anyone.com - All Right Reserved</p>
+        <p className='py-5 text-sm text-center'>Copyright 2025@ shrutisingh.com - All Right Reserved</p>
       </div>
     </div>
   )
