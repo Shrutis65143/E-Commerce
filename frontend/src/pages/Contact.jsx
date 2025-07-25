ad879fab118508339578d9ee85c9c385f2fdd0c7
Shruti Singh <168437828+Shrutis65143@users.noreply.github.com>
2025-07-25 21:36:26 +0530

    Update Contact.jsx

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -14,9 +14,9 @@ const Contact = () => {
           <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
           <div className='flex flex-col justify-center items-start gap-6'>
            <p className='font-semibold text-xl text-gray-600'>Our Store</p>
-           <p className='text-gray-500'>54089 Willms station <br />Suite 340,Washington,USA</p>
-           <p className='text-gray-500'>Tel: (789) 555-5678 <br />Email: sgum@gamil.com</p>
-           <p className='font-semibold text-xl text-gray-600'>Careers at forever</p>
+           <p className='text-gray-500'>540 Defence Colony<br />Suite 340,Model Town,DELHI</p>
+           <p className='text-gray-500'>Tel: (789) 555-5678 <br />Email: shrutisingh@gamil.com</p>
+           <p className='font-semibold text-xl text-gray-600'>Careers at anyone</p>
            <p className='text-gray-500'>Learn more about our teams and job openings</p>
            <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
           </div>
